d862430e1c61a574270c255ec1bf5fdcad696c26
ChaseHarding <chaseharding965@gmail.com>
2024-03-03 20:48:03 -0800

    Finished MoleContainer component

---
+++ MoleContainer.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import Mole from './src/Mole'
+import EmptyHill from './src/EmptyMoleSlot'
+
+const MoleContainer = (props) => {
+    let [ leMole, setLeMole] = useState(false)
+
+    const handleClick = (e) => {
+        props.setScore(props.score + 1)
+        setLeMole(false)
+    }
+
+    let displayMole = leMole ? <Mole setScore={props.setScore} toggle={setLeMole} handleClick={handleClick} /> : <EmptyHill toggle={setLeMole} />
+
+    return (
+        <div style={{'display': 'inline-block', 'width': '30vw'}}>
+            {displayMole}
+        </div>
+    )
+}
+
+export default MoleContainer
